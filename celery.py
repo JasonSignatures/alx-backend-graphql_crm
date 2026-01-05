59fbeffd11413327019031ea506f64bbb958e17e
Enobong Jackson <106785757+JasonSignatures@users.noreply.github.com>
2026-01-05 23:52:38 +0100

    Initialize Celery with Django settings

---
+++ celery.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# Default Django settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm.settings')
+
+app = Celery('crm')
+
+# Load config from Django settings
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-discover tasks from installed apps
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
